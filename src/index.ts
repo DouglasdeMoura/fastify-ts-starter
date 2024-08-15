2cba2b9587c2bd5983f6176c90dbf68cbf8ea880
Douglas Moura <douglas.ademoura@gmail.com>
2024-08-15 09:36:05 -0300

    feat: add global error handler

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,13 @@ const app = Fastify({
 
 app.register(appService)
 
+app.setErrorHandler(async (err, request, reply) => {
+  request.log.error({ err })
+  reply.code(err.statusCode || 500)
+
+  return { error: err.message }
+})
+
 // delay is the number of milliseconds for the graceful close to finish
 const closeListeners = closeWithGrace(
   { delay: env.FASTIFY_CLOSE_GRACE_DELAY },
